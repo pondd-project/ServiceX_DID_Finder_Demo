c665cb8205a7771c7a180ba845287460ee683773
Andrea Zonca <code@andreazonca.com>
2021-11-30 10:07:13 -0800

    add test scripts

---
+++ tests/simple_query.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # A Simple DID Finder
+from func_adl_servicex import ServiceXSourceUpROOT
+from servicex import ServiceXDataset
+
+sx_dataset = ServiceXDataset("demo://dataset1", backend_name='dev_uproot')
+ds = ServiceXSourceUpROOT(sx_dataset, "mini")
+
+data = ds.Select("lambda e: {'JetPT': e['jet_pt']}").AsAwkwardArray().value()
+
+data['JetPT']
